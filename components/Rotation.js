ac3e570c09987df9407d52596862fe80f4d283d7
PreetiSharma15 <pwhitehat6@gmail.com>
2021-09-07 12:52:37 +0530

    Rotation Reader Updated

--- components/Rotation.js
+++ components/Rotation.js
@@ -1,18 +1,18 @@
 //Terrain Rotation
 AFRAME.registerComponent("terrain-rotation-reader", {
   schema: {
-    speedOfRoation: { type: "number", default: 0 },
+    speedOfRotation: { type: "number", default: 0 },
   },
   init: function () {
     window.addEventListener("keydown", (e) => {
       if (e.key === "ArrowRight") {
-        if (this.data.speedOfRoation < 0.1) {
-          this.data.speedOfRoation += 0.01;
+        if (this.data.speedOfRotation < 0.1) {
+          this.data.speedOfRotation += 0.01;
         }
       }
       if (e.key === "ArrowLeft") {
-        if (this.data.speedOfRoation > -0.1) {
-          this.data.speedOfRoation -= 0.01;
+        if (this.data.speedOfRotation > -0.1) {
+          this.data.speedOfRotation -= 0.01;
         }
       }
     });
@@ -20,7 +20,7 @@ AFRAME.registerComponent("terrain-rotation-reader", {
   tick: function () {
     var mapRotation = this.el.getAttribute("rotation");
 
-    mapRotation.y += this.data.speedOfRoation;
+    mapRotation.y += this.data.speedOfRotation;
 
     this.el.setAttribute("rotation", {
       x: mapRotation.x,
@@ -33,17 +33,17 @@ AFRAME.registerComponent("terrain-rotation-reader", {
 //Plane rotation component
 AFRAME.registerComponent("plane-rotation-reader", {
   schema: {
-    speedOfRoation: { type: "number", default: 0 },
+    speedOfRotation: { type: "number", default: 0 },
     speedOfAscent: { type: "number", default: 0 }
   },
   init: function () {
     window.addEventListener("keydown", (e) => {
 
       //get the data from the attributes
-      this.data.speedOfRoation = this.el.getAttribute("rotation");      
+      this.data.speedOfRotation = this.el.getAttribute("rotation");      
       this.data.speedOfAscent = this.el.getAttribute("position");
 
-      var planeRotation = this.data.speedOfRoation;      
+      var planeRotation = this.data.speedOfRotation;      
       var planePosition = this.data.speedOfAscent;
 
       //control the attributes with the Arrow Keys
